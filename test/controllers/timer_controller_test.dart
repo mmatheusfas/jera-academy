15a54b5dc1d9b384d3809ea6c711bb5875850577
unknown <mfasmatheus@gmail.com>
2023-03-19 13:11:49 -0300

    Remove unecessary function test

--- test/controllers/timer_controller_test.dart
+++ test/controllers/timer_controller_test.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pomodoro_jera/controllers/timer_controller.dart';
 import 'package:pomodoro_jera/model/timer_model.dart';
@@ -7,24 +5,11 @@ import 'package:pomodoro_jera/model/timer_model.dart';
 void main() {
   var controller = TimerController();
   var timerModel = TimerModel(timerGoal: 2);
-  Timer? timer;
 
   test("Test if the function initializeDuration is working as expected", () {
     var result = controller.initializeDuration(timerModel.timerGoal, false);
 
     expect(result.inMinutes, 2);
-  });
-
-  test("Test if the function twoDigitsFormat is working as expected when the number is one digit", () {
-    var result = controller.twoDigitsFormater(5);
-
-    expect(result, '05');
-  });
-
-  test("Test if the function twoDigitsFormat is working as expected when the number is two digit", () {
-    var result = controller.twoDigitsFormater(12);
-
-    expect(result, '12');
   });
 
   test("Test if the function startTimer is working as expected, changing the timeStarted and activating the timer", () {
